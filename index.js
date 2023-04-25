fac0039231293ca228a04170e325534ff0623095
Tathagat017 <114250830+Tathagat017@users.noreply.github.com>
2023-04-25 23:50:01 +0530

    JWT sign and verify

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
 const express = require("express");
 const { connection } = require("./db.js");
 const { userRouter } = require("./routes/routes.js");
+
+const jwt = require("jsonwebtoken");
 const app = express();
 app.use(express.json());
 app.use("/users", userRouter);
+
+app.get("/", (req, res) => {
+  res.status(200).send("Home Page");
+});
+
+app.get("/contacts", (req, res) => {
+  res.status(200).send("Contact Page");
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).send("Contact Page");
+});
+//protected routes with unique and random token
+app.get("/movies", (req, res) => {
+  try {
+    jwt.verify(req.query.token, "secretkey", (err, decoded) => {
+      if (decoded) {
+        res.status(200).send({ data: "movie data" });
+      } else {
+        res.status(404).send({ error: "error in jwt" });
+      }
+    });
+  } catch (err) {
+    res.status(404).send({ error: "error encountered" });
+  }
+});
+
+app.get("/series", (req, res) => {
+  if (req.query.tokens.length > 0 && req.query.token === "tag123")
+    res.status(200).send("series Page");
+  else {
+    res.status(401).send("Login required");
+  }
+});
+
 app.listen(8080, "localhost", async () => {
   try {
     await connection;
--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,33 @@
 const express = require("express");
 const userRouter = express.Router();
+const jwt = require("jsonwebtoken");
 const { userModel } = require("../Model/userModel");
 userRouter.post("/register", async (req, res) => {
   //logic
-  res.status(200).send("registered successfully!");
+  try {
+    const user = new userModel(req.body);
+    await user.save();
+    res.status(200).send({ message: "registered successfully!" });
+  } catch (err) {
+    res.status(404).send({ message: err.message });
+  }
 });
 
 userRouter.post("/login", async (req, res) => {
-  //logic
-  res.status(200).send("login successful!");
+  const { email, password } = req.body;
+  var token = jwt.sign({ course: "backend" }, "secretkey");
+  try {
+    console.log(password, email);
+    let user = await userModel.findOne({ email, password });
+    console.log(user);
+    if (user) {
+      res.status(200).send({ msg: "Login successful", token: token });
+    } else {
+      res.status(404).send({ message: "credentials not found" });
+    }
+  } catch (err) {
+    res.status(404).send({ message: err.message });
+  }
 });
 
 module.exports = { userRouter };
